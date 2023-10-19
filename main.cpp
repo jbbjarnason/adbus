f6e38b42ce4ad4c6e3083ded3f8c8eff29f0e189
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-10-19 18:51:18 +0000

    still wip

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <printf.h>
 #include <regex>
 #include <type_traits>
 
@@ -37,6 +36,10 @@ public:
         [this, first_call = true,
          buffer_ptr = std::make_shared<std::remove_cvref_t<decltype(buffer)>>(std::forward<decltype(buffer)>(buffer))](
             auto& self, std::error_code err = {}, std::size_t size = 0) mutable {
+          if (err) {
+            self.complete(err, size);
+            return;
+          }
           if (first_call) {
             first_call = false;
             socket_.async_write_some(asio::buffer(*buffer_ptr), std::move(self));
@@ -48,13 +51,39 @@ public:
   }
 
   auto async_read_some(auto&& buffer, auto&& token) {
-    return asio::async_compose<decltype(token), void(std::error_code, std::shared_ptr<std::remove_cvref_t<decltype(buffer)>>)>(
+    return asio::async_compose<decltype(token),
+                               void(std::error_code, std::shared_ptr<std::remove_cvref_t<decltype(buffer)>>)>(
         [this, first_call = true,
          buffer_ptr = std::make_shared<std::remove_cvref_t<decltype(buffer)>>(std::forward<decltype(buffer)>(buffer))](
             auto& self, std::error_code err = {}, std::size_t size = 0) mutable {
+          if (err) {
+            self.complete(err, nullptr);
+            return;
+          }
           if (first_call) {
             first_call = false;
             socket_.async_read_some(asio::buffer(*buffer_ptr), std::move(self));
+            return;
+          }
+          self.complete(err, std::move(buffer_ptr));
+        },
+        token);
+  }
+
+  auto async_receive(auto&& buffer, auto&& token) {
+    return asio::async_compose<decltype(token),
+                               void(std::error_code, std::shared_ptr<std::remove_cvref_t<decltype(buffer)>>)>(
+        [this, first_call = true,
+         buffer_ptr = std::make_shared<std::remove_cvref_t<decltype(buffer)>>(std::forward<decltype(buffer)>(buffer))](
+            auto& self, std::error_code err = {}, std::size_t size = 0) mutable {
+          if (err) {
+            self.complete(err, nullptr);
+            return;
+          }
+          if (first_call) {
+            first_call = false;
+            buffer_ptr->resize(1024);
+            socket_.async_receive(asio::buffer(*buffer_ptr), std::move(self));
             return;
           }
           self.complete(err, std::move(buffer_ptr));
@@ -68,23 +97,46 @@ public:
   template <typename completion_token_t>
   auto external_authenticate(decltype(getuid()) user_id, completion_token_t&& token)
       -> asio::async_result<std::decay_t<completion_token_t>, void(std::error_code, std::size_t)>::return_type {
-    //  31303030 is ASCII decimal "1000" represented in hex, so
-    //  the client is authenticating as Unix uid 1000 in this example.
-    //
-    //  C: AUTH EXTERNAL 31303030
-    //  S: OK 1234deadbeef
-    //  C: BEGIN
+    enum struct state_e : std::uint8_t { begin, auth, done };
+    return asio::async_compose<completion_token_t, void(std::error_code, std::size_t)>(
+        [this, state = state_e::begin, user_id](auto& self, std::error_code err = {}, std::size_t size = 0) mutable {
+          //  31303030 is ASCII decimal "1000" represented in hex, so
+          //  the client is authenticating as Unix uid 1000 in this example.
+          //
+          //  C: AUTH EXTERNAL 31303030
+          //  S: OK 1234deadbeef
+          //  C: BEGIN
 
-    // The protocol is a line-based protocol, where each line ends with \r\n.
-    static constexpr std::string_view line_ending{ "\r\n" };
-    static constexpr std::string_view auth_command{ "AUTH" };
-    static constexpr std::string_view auth_mechanism{ "EXTERNAL" };
+          // The protocol is a line-based protocol, where each line ends with \r\n.
+          static constexpr std::string_view line_ending{ "\r\n" };
+          static constexpr std::string_view auth_command{ "AUTH" };
+          static constexpr std::string_view auth_mechanism{ "EXTERNAL" };
 
-    auto hex = ascii_to_hex(std::to_string(user_id));
+          if (err) {
+            self.complete(err, size);
+            return;
+          }
 
-    auto auth = fmt::format("{} {} {}{}", auth_command, auth_mechanism, hex, line_ending);
-
-    return async_write_some(std::move(auth), std::forward<decltype(token)>(token));
+          switch (state) {
+            case state_e::begin: {
+              state = state_e::auth;
+              async_write_some(std::array{ '\0' }, std::move(self));
+              break;
+            }
+            case state_e::auth: {
+              state = state_e::done;
+              auto hex = ascii_to_hex(std::to_string(user_id));
+              auto auth = fmt::format("{} {} {}{}", auth_command, auth_mechanism, hex, line_ending);
+              async_write_some(std::move(auth), std::move(self));
+              break;
+            }
+            case state_e::done: {
+              self.complete(err, size);
+              return;
+            }
+          }
+        },
+        token, socket_);
   }
 
 private:
@@ -118,14 +170,17 @@ int main() {
   path = std::regex_replace(path, std::regex(std::string{ asio::dbus::detail::unix_path_prefix }), "");
 
   asio::local::stream_protocol::endpoint ep{ path };
+
+  fmt::print("Connecting to {}\n", ep.path());
+
   socket.async_connect(ep, [&socket](auto&& ec) {
     if (ec) {
-      printf("error: %s\n", ec.message().c_str());
+      fmt::print("error: {}\n", ec.message());
       return;
     }
-    printf("connected\n");
+    fmt::print("connected\n");
     socket.external_authenticate([&socket](std::error_code err, std::size_t size) {
-      socket.async_read_some(std::string{}, [](std::error_code err, std::shared_ptr<std::string> buffer){
+      socket.async_receive(std::string{}, [](std::error_code err, std::shared_ptr<std::string> buffer) {
         fmt::print("read buffer{} err {}\n", buffer->size(), err.message());
       });
     });
