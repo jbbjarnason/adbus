18ac8e14b9a7924cd83b4a5cfa68c7492cf56821
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-06-19 18:50:25 +0000

    add incoming message queue handler, need to incorporate it though

--- main.cpp
+++ main.cpp
@@ -12,6 +12,16 @@
 #include <adbus/protocol/signature.hpp>
 #include <adbus/protocol/write.hpp>
 
+// todo properly use this dependency by namespacing it or use package manager
+#include <adbus/ext/boost/sam/condition_variable.hpp>
+namespace adbus {
+#ifdef BOOST_SAM_STANDALONE
+using condition_variable = sam::condition_variable;
+#else
+using condition_variable = boost::sam::condition_variable;
+#endif
+}  // namespace adbus
+
 namespace adbus {
 
 namespace api {
@@ -38,6 +48,83 @@ enum struct request_name_reply : std::uint32_t { unknown = 0, primary_owner = 1,
 
 
 namespace asio = boost::asio;
+
+namespace detail {
+
+class incoming_message_queue {
+public:
+  void on_message(is_header auto&& header, std::string&& message) {
+    std::scoped_lock lock{ mutex_ };
+    if (message_queue_.empty()) {
+      return;
+    }
+    switch (header.type) {
+      using enum protocol::header::message_type_e;
+      case method_return: {
+        auto const serial{ header.serial };
+        // use ranges::find_if
+        auto it = std::ranges::find_if(message_queue_, [serial](auto&& event) { return event->header.serial == serial; });
+        if (it != message_queue_.end()) {
+          (*it)->message = std::move(message);
+          (*it)->cv.notify_all();
+          message_queue_.erase(it);
+        }
+        break;
+      }
+      case error: {
+        // todo implement
+        break;
+      }
+      case signal: {
+        // todo implement
+        break;
+      }
+      case method_call: {
+        // todo implement
+        break;
+      }
+      case invalid: {
+        // todo implement
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+  
+  struct event {
+    protocol::header::header header{};
+    condition_variable cv;
+    std::string message{};
+    std::error_code err{};
+  };
+
+  auto async_wait(is_header auto&& header, asio::completion_token_for<void(std::error_code, std::string_view)> auto&& token) {
+    auto exe{ asio::get_associated_executor(token) };
+    auto new_event{ std::make_shared<event>(header, condition_variable{ exe}) };
+    std::scoped_lock lock{ mutex_ };
+    message_queue_.emplace_back(new_event);
+    return asio::async_compose<decltype(token), void(std::error_code, std::string_view)>(
+        [first_call = true, new_event](auto& self, std::error_code err = {}) mutable {
+          if (first_call) {
+            first_call = false;
+            new_event->cv_.async_wait(std::move(self));
+            return;
+          }
+          if (err) {
+            return self.complete(err);
+          }
+          self.complete(new_event->err, new_event->message);
+        },
+        token, exe);
+  }
+private:
+  std::vector<std::shared_ptr<event>> message_queue_;
+  std::mutex mutex_;
+};
+
+}
 
 template <typename Executor = asio::any_io_executor>
 class basic_dbus_socket {
@@ -289,7 +376,8 @@ int main() {
 
   adbus::basic_dbus_socket socket{ ctx };
 
-  std::string path{ getenv(adbus::env::session.data()) };
+  // example $ socat -v UNIX-LISTEN:/tmp/relay2.sock,fork UNIX-CONNECT:/run/user/1000/bus
+  std::string path{ "/tmp/relay2.sock" };//{ getenv(adbus::env::session.data()) };
   path = std::regex_replace(path, std::regex(std::string{ adbus::detail::unix_path_prefix }), "");
 
   asio::local::stream_protocol::endpoint ep{ path };
